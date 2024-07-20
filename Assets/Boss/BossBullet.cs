5dc839d5018d756b45355bad18a666f5ab3375af
Nagato <transcendentalliouville@gmail.com>
2024-07-20 13:33:23 +0900

    追従してくる弾幕を追加したうえで、弾幕をすこーしだけ薄くする

--- Assets/Boss/BossBullet.cs
+++ Assets/Boss/BossBullet.cs
@@ -5,14 +5,15 @@ using UnityEngine;
 public class BossBullet : MonoBehaviour
 {
     float span = 0.6f, delta = 0f, AngleParam = 0f, AngleDif = 1f, d;
+    float span2 = 6f, delta2 = 0;
 
     public static float PlayerR = 0.1f;
-    float BulletR = 0.1f;
+    float BulletR = 0.1f, FollowBulletR = 0.15f;
 
-    [SerializeField] GameObject bullet;
+    [SerializeField] GameObject bullet, followBullet;
     GameObject player;
 
-    Transform bullets;
+    Transform bullets, followBullets;
     
     void Start()
     {
@@ -20,6 +21,8 @@ public class BossBullet : MonoBehaviour
         this.player = GameObject.Find("Player");
         bullet = GameObject.Find("Bullets");
         bullets = new GameObject("Bullet").transform;
+        followBullet = GameObject.Find("FollowBullets");
+        followBullets = new GameObject("FollowBullet").transform;
     }
     
     void Update()
@@ -29,29 +32,106 @@ public class BossBullet : MonoBehaviour
             SceneChange.Result();
         }
         
-        this.delta += Time.deltaTime;
+        this.delta += Time.deltaTime; this.delta2 += Time.deltaTime;
 
+        // ’Ç]‚¶‚á‚È‚¢’e
         if (this.delta > this.span && BossHP.HP > 8f)
         {
             this.delta = 0f;
-            RadialBullet(45, 1);
+            RadialBullet(45, 1, bullet, bullets);
         }else if (this.delta > this.span && BossHP.HP > 6f)
         {
             this.span = 0.3f;
             this.delta = 0f;
-            RadialBullet(10, 2);
+            RadialBullet(10, 2, bullet, bullets);
         }
         else if(this.delta > this.span)
         {
+            this.span2 = 3f;
             this.span = 0.4f;
             this.delta = 0f;
-            RadialBullet(180, this.AngleDif);
+            RadialBullet(180, this.AngleDif, bullet, bullets);
             ++this.AngleDif;
             if (this.AngleDif > 360) this.AngleDif = 0;
         }
 
+        // ’Ç]‚µ‚Ä‚­‚é’e
+        if(this.delta2 > this.span2)
+        {
+            this.delta2 = 0f;
+            FollowBullet(followBullet, followBullets);
+        }
 
         // “–‚½‚è”»’è
+        CollisionDetection(BulletR, bullet, bullets);
+        CollisionDetection(FollowBulletR, followBullet, followBullets);
+
+    }
+
+
+
+
+    /**********************************
+     * 
+     * •úŽËó‚ÉL‚ª‚é‚²‚­ˆê”Ê“I‚È’e–‹
+     * 
+     **********************************/
+    void RadialBullet(float limit, float difference, GameObject bullet, Transform bullets)
+    {
+        this.AngleParam += this.AngleDif;
+        if (this.AngleParam >= limit) this.AngleDif = -difference;
+        if (this.AngleParam < 0f) this.AngleDif = difference;
+        
+        for (int angle = 0; angle < 20; ++angle)
+        {
+            InstBullet(transform.position, Quaternion.Euler(0f, 0f, angle * 18f + AngleParam), bullet, bullets);
+        }
+    }
+
+
+    /**********************************
+     * 
+     * ’Ç]‚µ‚Ä‚­‚é‚¤‚´‚¢‚â‚Â
+     * 
+     **********************************/
+    void FollowBullet(GameObject bullet, Transform bullets)
+    {
+        float angle, tmp;
+        tmp = (transform.position.y - this.player.transform.position.y) / (transform.position.x - this.player.transform.position.x);
+        angle = Mathf.Abs(Mathf.Atan(tmp)) * 180f / Mathf.PI + 180f;
+
+        InstBullet(transform.position, Quaternion.Euler(0f, 0f, angle), bullet, bullets);
+    }
+
+
+    /********************************
+     * 
+     * ’e–‹‚ð¶¬‚·‚éŠÖ”
+     * 
+     ********************************/
+    void InstBullet(Vector3 pos, Quaternion rotation, GameObject bullet, Transform bullets)
+    {
+        foreach (Transform t in bullets)
+        {
+            if (!t.gameObject.activeSelf)
+            {
+                t.SetPositionAndRotation(pos, rotation);
+                t.gameObject.SetActive(true);
+                return;
+            }
+        }
+
+        Instantiate(bullet, pos, rotation, bullets);
+    }
+
+
+    /**********************************
+     * 
+     * “–‚½‚è”»’è‚ð‚·‚éŠÖ”
+     * 
+     **********************************/
+    void CollisionDetection(float BulletR, GameObject bullet, Transform bullets)
+    {
         Vector2 PlayerPosition = this.player.transform.position;
         foreach (Transform t in bullets)
         {
@@ -60,9 +140,9 @@ public class BossBullet : MonoBehaviour
                 Vector2 BulletPosition = t.position;
                 Vector2 dir = BulletPosition - PlayerPosition;
                 float d = dir.magnitude;
-                
+
                 // ’e–‹‚É‚ß‚Á‚¿‚á‹ß‚©‚Á‚½‚çScore‚ð‰ÁŽZ
-                if(d < PlayerR + BulletR + 0.2f)
+                if (d < PlayerR + BulletR + 0.2f)
                 {
                     PlayerPrefs.SetInt("Score", PlayerPrefs.GetInt("Score") + 30);
                     PlayerPrefs.Save();
@@ -78,44 +158,5 @@ public class BossBullet : MonoBehaviour
                 }
             }
         }
-    }
-
-
-    /**********************************
-     * 
-     * •úŽËó‚ÉL‚ª‚é‚²‚­ˆê”Ê“I‚È’e–‹
-     * 
-     **********************************/
-    void RadialBullet(float limit, float difference)
-    {
-        this.AngleParam += this.AngleDif;
-        if (this.AngleParam >= limit) this.AngleDif = -difference;
-        if (this.AngleParam < 0f) this.AngleDif = difference;
-        
-        for (int angle = 0; angle < 32; ++angle)
-        {
-            InstBullet(transform.position, Quaternion.Euler(0f, 0f, angle * 11.25f + AngleParam));
-        }
-    }
-
-
-    /********************************
-     * 
-     * ’e–‹‚ð¶¬‚·‚éŠÖ”
-     * 
-     ********************************/
-    void InstBullet(Vector3 pos, Quaternion rotation)
-    {
-        foreach (Transform t in bullets)
-        {
-            if (!t.gameObject.activeSelf)
-            {
-                t.SetPositionAndRotation(pos, rotation);
-                t.gameObject.SetActive(true);
-                return;
-            }
-        }
-
-        Instantiate(bullet, pos, rotation, bullets);
     }
 }
--- Assets/Boss/BossController.cs
+++ Assets/Boss/BossController.cs
@@ -6,10 +6,6 @@ using UnityEngine.SceneManagement;
 
 public class BossController : MonoBehaviour
 {
-    // HP of a boss
-    float HP = 20f, MaxHP = 20f;
-
-    public Text Score;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,27 +15,6 @@ public class BossController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(HP <= 0)
-        {
-            PlayerPrefs.SetInt("__STAGE__", PlayerPrefs.GetInt("__STAGE__") + 1);
-            PlayerPrefs.Save();
 
-            SceneManager.LoadScene("Stage" + PlayerPrefs.GetInt("__STAGE__").ToString());
-        }
-    }
-
-    void OnCollisionEnter2D(Collision2D collision)
-    {
-        --HP;
-
-        /*“¾“_‚Ì‰ÁŽZ*/
-        PlayerPrefs.SetInt("__SCORE__", PlayerPrefs.GetInt("__SCORE__") + 100);
-        PlayerPrefs.Save();
-
-        Score.text = PlayerPrefs.GetInt("__SCORE__").ToString();
-
-
-        GameObject director = GameObject.Find("GameDirector");
-        director.GetComponent<GameDirector>().DecreaseBossHP(HP, MaxHP);
     }
 }
